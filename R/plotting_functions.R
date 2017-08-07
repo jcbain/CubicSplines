e0cef7232896d5bae4a6cc891f1597870fd85043
James Bain <jamescbain@gmail.com>
2017-08-07 11:44:56 -0500

    write function to plot ggplot2 cubic spline

---
+++ R/plotting_functions.R
@@ -0,0 +1,37 @@
+#' @rdname stat_cubic_spline
+#' @format NULL
+#' @usage NULL
+#' @importFrom ggplot2 ggproto Stat
+#' @export
+
+StatCubicSpline <- ggplot2::ggproto("StatCubicSpline", ggplot2::Stat,
+                                    required_aes = c("x"),
+                                    compute_group = function(data, scales, minval, maxval, break_size) {
+                                      grid <- data.frame(x = data$x)
+
+                                      spline_funct <- cubicsplines::cubic_spline(grid$x,min=minval, max = maxval, break_size = break_size )
+                                      grid$y <- cubicsplines::spline_adjustment(grid$x, spline_funct(grid$x,deriv = 1))
+
+                                      grid
+                                    })
+
+#' Plot the cubic spline density
+#'
+#' This function is to be added as a stat_ to a ggplot2 plot.
+#' It creates a density plot calculated from the cubic spline
+#' of the cumulative frequency of a variable.
+#' @param minval The minimum value in a range of values
+#' @param maxval The maximum value in a range of values
+#' @param break_size The size of each break
+#' @rdname stat_cubicspline
+#' @importFrom ggplot2 layer
+#' @export
+stat_cubicspline <- function(mapping = NULL, data = NULL, geom = "line",
+                             position = "identity", na.rm = FALSE, show.legend = NA,
+                             inherit.aes = TRUE, ...) {
+  ggplot2::layer(
+    stat = StatCubicSpline, data = data, mapping = mapping, geom = geom,
+    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, ...)
+  )
+}
